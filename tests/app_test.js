0a21bcae197a67b9819bd299c328d98bccb17b75
VimalKTC <40485458+VimalKTC@users.noreply.github.com>
2018-06-28 23:28:38 +0530

    d
    d

--- tests/app_test.js
+++ tests/app_test.js
@@ -10,12 +10,11 @@ describe('Basic routes tests', function() {
 
     it('GET to / should return 200', function(done){
 
-        expect(1+1).to.equal(2);
+
 
     })
 
     it('GET to /pagecount should return 200', function(done){
-        expect(1+1).to.equal(2);
 
     })
 })
