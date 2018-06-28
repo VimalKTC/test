acdc2fe7923875d80ea6ccb3ebf550bf82459760
VimalKTC <40485458+VimalKTC@users.noreply.github.com>
2018-06-28 23:00:50 +0530

    k
    k

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,10 +1,11 @@
-var chai     = require('chai'),
+var server   = require('server'),
+    chai     = require('chai'),
     chaiHTTP = require('chai-http'),
     should   = chai.should();
 
 chai.use(chaiHTTP);
 
-reqServer = process.env.HTTP_TEST_SERVER;
+reqServer = process.env.HTTP_TEST_SERVER || server
 
 describe('Basic routes tests', function() {
 
